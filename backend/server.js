d17b6bce3d0071cfe3267b8e5a14846afe8438b7
arcadegofan105-sketch <arcadegofan105@gmail.com>
2026-01-12 20:16:51 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -190,7 +190,7 @@ function makeDepositId() {
 
 // ===== Promo config =====
 const PROMOS = {
-  WheelTon: 1,
+  Free05: 0.5,
   Admintestcodesss: 50,
 };
 
@@ -472,3 +472,4 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => console.log("✅ Listening on", PORT));
+
